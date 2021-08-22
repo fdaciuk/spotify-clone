1598f701b4607434bf9b48caeba9305d50370eae
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-08-22 16:39:59 -0300

    Importanto o estilo global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../src/styles/theme/globals'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
