defa288806d8386617ff1003d7e535ee91520d69
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-08-23 06:19:36 -0300

    Base do projeto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+
+/* ------| Componentes |------ */
+import { AudioPlayer } from '../src/components/Player'
+import { Sidebar } from '../src/components/Sidebar'
+
+/* ------| Estilos |------ */
+import { Container, Main, Wrapper, WrapperBox } from '../src/styles/pages/layout'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      Olá mundo
-    </div>
+    <Wrapper>
+      <Head>
+        <title>Spotify - Web player (redesigned)</title>
+      </Head>
+      <WrapperBox>
+        <Container>
+          <Sidebar />
+          <Main>
+            Olá
+          </Main>
+        </Container>
+        <AudioPlayer />
+      </WrapperBox>
+    </Wrapper>
   )
 }
 
