fdd7baeeccfdc877be9056c4d21a87d8495c40e8
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-08-23 06:18:40 -0300

    Componente Options

---
+++ src/components/Player/Options/index.tsx
@@ -0,0 +1,12 @@
+/* ------| Estilos |------ */
+import { Box, OptionsButtons } from "./styles"
+
+export const Options = () => {
+  return (
+    <Box>
+      <OptionsButtons>
+        
+      </OptionsButtons>
+    </Box>
+  )
+}
---
+++ src/components/Player/Options/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Box = styled.div `
+  width: 25%;
+  height: 100%;
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 0;
+  position: relative;
+`
+
+export const OptionsButtons = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
