61eba3f412359068526e8aea6899f919cf78576e
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-08-23 06:53:03 -0300

    Criação do componente Options

--- src/components/Player/Options/styles.ts
+++ src/components/Player/Options/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components'
 export const Box = styled.div `
   width: 25%;
   height: 100%;
-  display: block;
+  display: flex;
+  align-items: center;
   flex-grow: 0;
   flex-shrink: 0;
   position: relative;
@@ -14,4 +15,67 @@ export const OptionsButtons = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  padding-right: 32px;
+`
+
+export const OptionsButton = styled.button`
+  padding: 6px;
+  border: 0;
+  background-color: transparent;
+  cursor: pointer;
+  color: ${props => props.theme.colors.text.secondary};
+
+  &:hover {
+    color: ${props => props.theme.colors.text.primary};
+  }
+`
+
+export const OptionsAudioProgress = styled.div`
+  width: 112px;
+  height: 5px;
+  margin-top: -3px;
+  border-radius: 999px;
+  background-color: ${props => props.theme.background.secondary};
+`
+
+export const OptionsAudioProgressBar = styled.div`
+  width: 40%;
+  height: 100%;
+  border-radius: 99px;
+  display: block;
+  position: relative;
+  background-color: ${props => props.theme.background.tertiary};
+
+  :after {
+    content: '';
+    width: 13px;
+    height: 13px;
+    border-radius: 999px;
+    position: absolute;
+    top: -4px; right: -4px;
+    background-color: ${props => props.theme.colors.text.primary};
+    visibility: hidden;
+    opacity: 0;
+  }
+`
+
+export const OptionsAudio = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  padding: 0 12px;
+  position: relative;
+
+  :hover ${OptionsButton} {
+    color: ${props => props.theme.colors.text.primary};
+  }
+
+  :hover ${OptionsAudioProgress} ${OptionsAudioProgressBar} {
+    background-color: ${props => props.theme.colors.primary};
+
+    &:after {
+      visibility: visible;
+      opacity: 1;
+    }
+  }
 `
